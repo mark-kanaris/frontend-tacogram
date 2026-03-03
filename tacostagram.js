8201a54f7d461922637f88d411190f83078117e2
mark-kanaris <mark.kanaris@kellogg.northwestern.edu>
2026-03-03 17:29:04 +0000

    added json for front end, need to ask ai to make html for view

--- tacostagram.js
+++ tacostagram.js
@@ -1,6 +1,6 @@
 function getPostsFromAPI() {
   // Replace this with the URL of the JSON API that returns an array of image URLs
-  const url = 'YOUR_URL_GOES_HERE';
+  const url = 'https://bug-free-yodel-x576x7gxq756c6q6w-3000.app.github.dev/posts.json';
   if (url == 'YOUR_URL_GOES_HERE') {
     alert('Error: Replace url value in tacostagram.js')
   }
